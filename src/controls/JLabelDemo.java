c75104ab78d8d6c8b38766604321a7e8b94e1a98
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-23 00:45:30 +0700

    New Commit

--- src/controls/JLabelDemo.java
+++ src/controls/JLabelDemo.java
@@ -18,9 +18,14 @@ public class JLabelDemo {
     }
     private void prepareGUI() {
         this.mainFrame = new JFrame("Java Jlabel Demo");
-
+        this.mainFrame.setSize(800,400);
+        this.mainFrame.setLayout(new GridLayout(30,1));
+    }
+    private void showLabelDemo() {
+        this.mainFrame.setVisible(true);
     }
     public static void main(String[] args) {
-
+        JLabelDemo jLabelDemo = new JLabelDemo();
+        jLabelDemo.showLabelDemo();
     }
 }
